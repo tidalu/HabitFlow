cd365ca02aa335d27b32b28cd01da84450417377
Tidalu <ul0271069@edu.uni.lodz.pl>
2026-05-23 11:40:28 +0200

    fix: protected addding logs

--- src/routes/logs.route.ts
+++ src/routes/logs.route.ts
@@ -3,7 +3,7 @@ import createLogHandler from '#controllers/habits/habitlogs/createLog.controller
 import getLogsHandler from '#controllers/habits/habitlogs/getLogs.controller.js'
 import { isAuthenticated } from '#middlewares/auth.js'
 const router = express.Router()
-
+router.use(isAuthenticated)
 router.post('/:id/logs', createLogHandler)
 router.get('/:id/logs', getLogsHandler)
 
